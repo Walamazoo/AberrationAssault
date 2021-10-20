62fbe1de627ea72a7299d1ef5102774adf5de993
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2021-10-19 22:53:49 -0400

    Bullet now destroys itself on collision and leaves a small effect

---
+++ Aberration Assault/Assets/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code destruction on collision created based on https://www.youtube.com/watch?v=LNLVOjbrQj4
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] GameObject _hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(_hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1f);
+        Destroy(gameObject);
+    }
+}
