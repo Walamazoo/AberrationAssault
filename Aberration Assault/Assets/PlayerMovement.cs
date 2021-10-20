e510e3339a3a1e07bc5b2ee92ee7a8677a5300c1
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2021-10-19 22:32:46 -0400

    Player now aims at mouse. Added bullet sprite

--- Aberration Assault/Assets/PlayerMovement.cs
+++ Aberration Assault/Assets/PlayerMovement.cs
@@ -8,16 +8,25 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float _moveSpeed = 5f;
     [SerializeField] Rigidbody2D _rb;
+    [SerializeField] Camera _cam;
+
     Vector2 _movement;
+    Vector2 _mousePos;
   
     void Update()
     {
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
+
+        _mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
     }
     
     void FixedUpdate()
     {
         _rb.MovePosition(_rb.position + _movement * _moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = _mousePos - _rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        _rb.rotation = angle;
     }
 }
