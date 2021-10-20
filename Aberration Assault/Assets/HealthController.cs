9c56e622241edec4c3e66939ecdd8faf6aafa1c3
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2021-10-19 23:40:54 -0400

    Player now looses health when touching an enemy

--- Aberration Assault/Assets/HealthController.cs
+++ Aberration Assault/Assets/HealthController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class HealthController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] GameObject[] _hearts;
+    int _healthIndex = 0;
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+       if(collision.gameObject.CompareTag("Enemy") == true)
+       {
+            _hearts[_healthIndex].SetActive(false);
+            _healthIndex++;
+            if(_healthIndex > 2)
+            {
+                //GAME OVER
+            }
+       }
     }
 }
