0245f455c6dd5c1b0a5a82e05dc329f50ce85804
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2021-12-07 17:39:21 -0500

    Spit bullets work, additional enemies in scene

--- Aberration Assault/Assets/Scripts/BigEnemyBullet.cs
+++ Aberration Assault/Assets/Scripts/BigEnemyBullet.cs
@@ -6,6 +6,8 @@ public class BigEnemyBullet : MonoBehaviour
 {
     [SerializeField] float _speed;
 
+    [SerializeField] float _bulletForce = 10f;
+
     [SerializeField] GameObject _splitBulletPrefab;
 
     private Transform _player;
@@ -34,13 +36,23 @@ public class BigEnemyBullet : MonoBehaviour
 
     void DestroyProjectile()
     {
-        /*
-        for (int i=0;i<8;i++)
-        {
-            GameObject split = Instantiate(_splitBulletPrefab, transform.position, Quaternion.Euler((45 * i), 0, 0));
-        }
-        */
-        
+        GameObject split = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1f) * _bulletForce, ForceMode2D.Impulse);
+        GameObject split2 = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split2.GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, 1f) * _bulletForce, ForceMode2D.Impulse);
+        GameObject split3 = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split3.GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, 0) * _bulletForce, ForceMode2D.Impulse);
+        GameObject split4 = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split4.GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, -1f) * _bulletForce, ForceMode2D.Impulse);
+        GameObject split5 = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split5.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -1f) * _bulletForce, ForceMode2D.Impulse);
+        GameObject split6 = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split6.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1f, -1f) * _bulletForce, ForceMode2D.Impulse);
+        GameObject split7 = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split7.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1f, 0) * _bulletForce, ForceMode2D.Impulse);
+        GameObject split8 = Instantiate(_splitBulletPrefab, transform.position, Quaternion.identity);
+        split8.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1f, 1f) * _bulletForce, ForceMode2D.Impulse);
+
         Destroy(gameObject);
     }
 }
