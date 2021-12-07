c61c2081fd41f661d99c74f07a8d6c43268a6002
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2021-12-07 03:36:59 -0500

    Enemies now drop crystal materials. Scene transitions

--- Aberration Assault/Assets/Scripts/SceneChanger.cs
+++ Aberration Assault/Assets/Scripts/SceneChanger.cs
@@ -11,8 +11,17 @@ public class SceneChanger : MonoBehaviour
     }
     public void LoadLevel()
     {
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene("Level 1");
+    }
 
+    public void LoadLevelTwo()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadBossLevel()
+    {
+        SceneManager.LoadScene("Boss Level");
     }
 
     public void Quit()
