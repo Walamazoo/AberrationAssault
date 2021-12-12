b6965dfd2fc762385cedb76aea0853d001d1e307
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2021-12-12 18:17:15 -0500

    Laser is upgradable

--- Aberration Assault/Assets/Scripts/PauseMenu.cs
+++ Aberration Assault/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -8,8 +10,20 @@ public class PauseMenu : MonoBehaviour
 
     [SerializeField] bool _isPaused = false;
 
+    [SerializeField] MaterialController _materialController;
+
+    [SerializeField] Shooting _shooting;
+
+    [SerializeField] int _laserUpgradeCost;
+    [SerializeField] TextMeshProUGUI _laserUpgradeCostDisplay;
+    [SerializeField] GameObject[] _laserUpgrades;
+    int _laserUpgradeIndex = 0;
+
     void Start()
     {
+        _laserUpgradeCost = 2;
+        _laserUpgradeCostDisplay.text = _laserUpgradeCost.ToString();
+
         _pauseMenu.SetActive(false);
         _isPaused = false;
         Time.timeScale = 1;
@@ -31,6 +45,26 @@ public class PauseMenu : MonoBehaviour
                 Time.timeScale = 1;
                 _isPaused = false;
             } 
+        }
+    }
+
+    public void UpgradeLaser()
+    {
+        if (_laserUpgradeIndex <= 2)
+        {
+            if (_materialController._materialAmount >= _laserUpgradeCost)
+            {
+                _materialController.UpdateMaterialAmount(-_laserUpgradeCost);
+                _laserUpgradeCost += 2;
+                _laserUpgradeCostDisplay.text = (_laserUpgradeCost).ToString();
+                _laserUpgrades[_laserUpgradeIndex].GetComponent<Image>().color = new Color(12, 156, 229, 255);
+                _laserUpgradeIndex += 2;
+                if (_laserUpgradeIndex > 2)
+                {
+                    _laserUpgradeCostDisplay.text = "";
+                }
+                _shooting._bulletDamage += 1;
+            }
         }
     }
 }
